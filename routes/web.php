4cf61f7dd357d315be3f9b801576a04f843a31d5
Milan <miksan890@gmail.com>
2019-08-14 17:08:03 +0200

    adds author resource

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::post('/books', 'BooksController@store');
+Route::patch('/books/{book}', 'BooksController@update');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::post('author', 'AuthorsController@store');
