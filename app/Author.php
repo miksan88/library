f328a31d6b7b1e1d1b2b55b04a9903b0034ecad7
Milan <miksan890@gmail.com>
2019-08-27 09:59:13 +0200

    Se 3

--- app/Author.php
+++ app/Author.php
@@ -14,4 +14,5 @@ class Author extends Model
     {
         $this->attributes['dob'] = Carbon::parse($dob);
     }
+
 }
--- app/Book.php
+++ app/Book.php
@@ -14,6 +14,29 @@ class Book extends Model
         return '/books/' . $this->id;
     }
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
 
     public function setAuthorIdAttribute($author)
     {
@@ -22,4 +45,8 @@ class Book extends Model
         ]))->id;
     }
 
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
